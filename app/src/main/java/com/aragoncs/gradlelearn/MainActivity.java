8538cefbec606442cf0223e0277583cad5acce25
zhangfengzhou <1120335370@qq.com>
2016-11-13 20:00:09 +0800

    TASK-002:学习pull request知识

--- app/src/main/java/com/aragoncs/gradlelearn/MainActivity.java
+++ app/src/main/java/com/aragoncs/gradlelearn/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e("info","学习Gradle知识");
+        Log.e("info","学习Pull request知识");
     }
 }
