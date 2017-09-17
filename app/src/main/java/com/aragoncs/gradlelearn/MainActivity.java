610b43942fd2840b398fd0067f10418da83b36a1
zhang fengzhou <1120335370@qq.com>
2017-09-17 22:50:56 +0800

    利用不同的依赖module对资源进行替换和逻辑判断转换

--- app/src/main/java/com/aragoncs/gradlelearn/MainActivity.java
+++ app/src/main/java/com/aragoncs/gradlelearn/MainActivity.java
@@ -3,14 +3,23 @@ package com.aragoncs.gradlelearn;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvAppFlavor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvAppFlavor = (TextView) findViewById(R.id.tv_app_flavor);
         Log.e("info","学习Gradle知识");
         Log.e("info","学习Pull request知识");
+        if (BuildConfig.OEM.equals("OEMA")){
+            tvAppFlavor.setText("OEMA");
+        }else{
+            tvAppFlavor.setText("OEMB");
+        }
     }
 }
