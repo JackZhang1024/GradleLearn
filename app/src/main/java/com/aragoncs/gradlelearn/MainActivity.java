47280c52635ba35bbe3120896390cc3e0b10b1e5
zhangfengzhou <1120335370@qq.com>
2018-11-09 20:50:45 +0800

    Gradle更新

--- app/src/main/java/com/aragoncs/gradlelearn/MainActivity.java
+++ app/src/main/java/com/aragoncs/gradlelearn/MainActivity.java
@@ -7,19 +7,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvAppFlavor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvAppFlavor = (TextView) findViewById(R.id.tv_app_flavor);
-        Log.e("info","学习Gradle知识");
-        Log.e("info","学习Pull request知识");
-        if (BuildConfig.OEM.equals("OEMA")){
-            tvAppFlavor.setText("OEMA");
-        }else{
-            tvAppFlavor.setText("OEMB");
-        }
     }
 }
